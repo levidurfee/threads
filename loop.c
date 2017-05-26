3ddff0fdedfa31aceab694689c6484e62c209b21
Levi Durfee <levi.durfee@gmail.com>
2017-05-26 18:47:12 -0400

    populate stack

--- loop.c
+++ loop.c
@@ -9,6 +9,9 @@ struct prime_data {
     long exponent;
 };
 
+struct prime_data pd;
+
+
 void *prime(void *ptr) {
     printf("Thread number %ld\n", (long)pthread_self());
     long i = 0;
@@ -16,10 +19,17 @@ void *prime(void *ptr) {
 }
 
 int main() {
+    int i, j, k, l;
+    Stack s;
+    init(&s, 0x65536); /* Stack of maximum height 0x65536 */
+    for(l=0; l<20; l++) {
+        push(&s, l + 2);
+    }
+
     pthread_t thread_id[NUM_THREADS];
-    int i, j, k;
-    for(k=0; k<100; k++) {
+    for(k=0; k<20; k++) {
         for(i=0; i < NUM_THREADS; i++) {
+            pd.exponent = pop(&s);
             pthread_create( &thread_id[i], NULL, prime, NULL );
         }
         for(j=0; j < NUM_THREADS; j++) {
