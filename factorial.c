2bf18ffad66c3e49ceed8893dd8f7f8d92504ad3
Levi Durfee <levi.durfee@gmail.com>
2017-05-25 20:49:04 -0400

    work on threads

--- factorial.c
+++ factorial.c
@@ -3,6 +3,14 @@
 
 #define NUM_THREADS 8
 
+struct thread_data {
+    int thread_id;
+    int number;
+    int sum;
+};
+
+struct thread_data thread_data_array[NUM_THREADS];
+
 long int factorial(int n);
 void *PrintHello(void *threadid);
 
@@ -17,10 +25,10 @@ int main() {
     long t;
     for(t=0; t<NUM_THREADS; t++){
        printf("In main: creating thread %ld\n", t);
-       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+       thread_data_array[t].thread_id = t;
+       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)&thread_data_array[t]);
        if (rc){
           printf("ERROR; return code from pthread_create() is %d\n", rc);
-          exit(-1);
        }
     }
 
@@ -35,9 +43,10 @@ long int factorial(int n) {
         return 1;
 }
 
-void *PrintHello(void *threadid) {
-    long tid;
-    tid = (long)threadid;
+void *PrintHello(void *threadarg) {
+    struct thread_data *my_data;
+    my_data = (struct thread_data *) threadarg;
+    long int tid = my_data->thread_id;
     printf("Hello World! It's me, thread #%ld!\n", tid);
     pthread_exit(NULL);
 }
