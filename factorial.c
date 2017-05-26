e1c4ef3284b3da2938bd9412347c2de21d20f6e6
Levi Durfee <levi.durfee@gmail.com>
2017-05-25 21:00:30 -0400

    get return working

--- factorial.c
+++ factorial.c
@@ -3,50 +3,20 @@
 
 #define NUM_THREADS 8
 
-struct thread_data {
-    int thread_id;
-    int number;
-    int sum;
-};
-
-struct thread_data thread_data_array[NUM_THREADS];
-
-long int factorial(int n);
-void *PrintHello(void *threadid);
+void *myThread()
+{
+    printf("Thread #%u working on task1\n", (int)pthread_self());
+    return (void *) 42;
+}
 
 int main() {
-    long int r;
-    r = factorial(5);
-    printf("%li\n", r);
+    pthread_t tid;
+    void *status;
 
-    pthread_t threads[NUM_THREADS];
+    pthread_create(&tid, NULL, myThread, NULL);
+    pthread_join(tid, &status);
 
-    int rc;
-    long t;
-    for(t=0; t<NUM_THREADS; t++){
-       printf("In main: creating thread %ld\n", t);
-       thread_data_array[t].thread_id = t;
-       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)&thread_data_array[t]);
-       if (rc){
-          printf("ERROR; return code from pthread_create() is %d\n", rc);
-       }
-    }
+    printf("%d\n",(int)status);
 
-    /* Last thing that main() should do */
-    pthread_exit(NULL);
-}
-
-long int factorial(int n) {
-    if (n >= 1)
-        return n*factorial(n-1);
-    else
-        return 1;
-}
-
-void *PrintHello(void *threadarg) {
-    struct thread_data *my_data;
-    my_data = (struct thread_data *) threadarg;
-    long int tid = my_data->thread_id;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
-    pthread_exit(NULL);
+    return 0;
 }
